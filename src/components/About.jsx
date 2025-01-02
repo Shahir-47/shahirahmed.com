0077abe1276b389743fca568a516416f52d1de79
Shahir Ahmed <ahmeds@dickinson.edu>
2025-01-01 21:34:00 -0500

    Add About and Projects components with detailed content and tools showcase

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,144 @@
+import Shahir from "../assets/shahir.png";
+import GitSocial from "../assets/gitSocial.svg";
+import Linked from "../assets/linked.svg";
+import Docker from "../assets/docker.svg";
+import Sql from "../assets/sql.svg";
+import Eclipse from "../assets/eclipse.svg";
+import Lua from "../assets/lua.svg";
+import Vue from "../assets/vue.svg";
+import Cypress from "../assets/cypress.svg";
+import Vscode from "../assets/vscode.svg";
+import C from "../assets/c.svg";
+import Npm from "../assets/npm.svg";
+import Babel from "../assets/babel.svg";
+import Prettier from "../assets/prettier.svg";
+import Eslint from "../assets/eslint.svg";
+import Webpack from "../assets/webpack.svg";
+import Jest from "../assets/jest.svg";
+import Git from "../assets/git.svg";
+import Python from "../assets/python.svg";
+import Java from "../assets/java.svg";
+import Linux from "../assets/linux.svg";
+import Html from "../assets/html.svg";
+import Css from "../assets/css.svg";
+import Js from "../assets/js.svg";
+import Matlab from "../assets/matlab.svg";
+
+const tools = [
+	{ src: Html, title: "HTML" },
+	{ src: Css, title: "CSS" },
+	{ src: Js, title: "JavaScript" },
+	{ src: Git, title: "Git" },
+	{ src: Npm, title: "NPM" },
+	{ src: Vscode, title: "VS Code" },
+	{ src: Docker, title: "Docker" },
+	{ src: Linux, title: "Linux" },
+	{ src: Python, title: "Python" },
+	{ src: Java, title: "Java" },
+	{ src: Sql, title: "SQL" },
+	{ src: Webpack, title: "Webpack" },
+	{ src: Vue, title: "Vue.js" },
+	{ src: Babel, title: "Babel" },
+	{ src: Jest, title: "Jest" },
+	{ src: Prettier, title: "Prettier" },
+	{ src: Eslint, title: "Eslint" },
+	{ src: Cypress, title: "Cypress" },
+	{ src: C, title: "C++" },
+	{ src: Lua, title: "Lua" },
+	{ src: Eclipse, title: "Eclipse" },
+	{ src: Matlab, title: "Matlab" },
+];
+
 const About = () => {
-	return <div>About me section.</div>;
+	return (
+		<div className="about-page">
+			<div className="about-container">
+				<div className="about-text-container">
+					<h1 className="about-me">
+						About <span>Me</span>
+					</h1>
+				</div>
+				<div className="about-me-div">
+					<div className="about-me-text-div">
+						<div className="paragraph">
+							<p className="about-me-text">
+								Hello! I&apos;m Shahir, a dedicated Computer Science and
+								Mathematics student at Dickinson College, where I&apos;m
+								mastering the art of full-stack development, with a keen eye on
+								creating innovative web solutions. My academic journey is richly
+								complemented by hands-on experience, especially noted during my
+								transformative internship at 84 Lumber. There, I took the lead
+								in modernizing technology by converting 50,000 lines of legacy
+								code into contemporary and scalable Java and Python
+								applications, showcasing my proficiency in these languages and
+								my knack for efficient problem-solving.
+							</p>
+							<p className="about-me-text">
+								My commitment to tech excellence is demonstrated through a
+								series of challenging projects. I&apos;ve dedicated myself to
+								developing a variety of applications, from interactive games to
+								dynamic web applications, each serving as a platform for
+								learning and applying new technologies and coding techniques.
+								These projects, available in my active GitHub repository,
+								highlight my dedication to coding excellence and my passion for
+								exploring the boundaries of web and software development.
+							</p>
+							<p className="about-me-text">
+								As I continue to evolve in this dynamic field, my goal is to
+								leverage my skills in a professional setting where I can
+								contribute to meaningful tech solutions and further hone my
+								expertise. Whether working solo or as part of a team, I approach
+								every project with a meticulous analytical mindset, constantly
+								striving to exceed expectations and drive innovation in the tech
+								space.
+							</p>
+							<p className="about-me-text">
+								Let&apos;s connect to explore opportunities in web development,
+								collaborate on exciting projects, or simply to exchange insights
+								on the latest in technology!
+							</p>
+						</div>
+						<div className="social-icons">
+							<a
+								href="https://github.com/Shahir-47"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img src={GitSocial} alt="GitHub" />
+							</a>
+							<a
+								href="https://www.linkedin.com/in/shahir47/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img src={Linked} alt="LinkedIn" />
+							</a>
+						</div>
+					</div>
+					<img src={Shahir} alt="Shahir" className="about-me-picture" />
+				</div>
+			</div>
+			<div className="second-div">
+				<div className="second-div-text">
+					<p>
+						<span className="highlight">Tools</span> and{" "}
+						<span className="highlight">Technologies</span> I&apos;ve worked
+						with
+					</p>
+				</div>
+				<div className="carousel-container">
+					<div className="carousel">
+						{tools.map((tool, index) => (
+							<div className="carousel-item" key={index}>
+								<img src={tool.src} alt={tool.title} />
+								<p>{tool.title}</p>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default About;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,107 @@
+import PropTypes from "prop-types";
+import Todo from "../assets/todo.png";
+import Battleship from "../assets/battleship.png";
+import Weather from "../assets/weather.png";
+import Tictac from "../assets/tictac.png";
+import Calculator from "../assets/calculator.png";
+import Restaurant from "../assets/restaurant.png";
+import Etch from "../assets/etch.png";
+
+const projectsData = [
+	{
+		image: Todo,
+		title: "Todo List",
+		description:
+			"Todo List is a powerful task management application that helps you stay organized and productive. Manage tasks, projects, and notes effortlessly. Features include priority levels, due dates, search functionality, and dark/light theme. Get started and boost your productivity today!",
+		liveDemo: "https://shahir-47.github.io/Todo-List/",
+		sourceCode: "https://github.com/Shahir-47/Todo-List",
+	},
+	{
+		image: Battleship,
+		title: "Battleship",
+		description:
+			"A web-based Battleship game with a strategic AI. Players engage in classic naval warfare against a computer. Code is open for contributions and tests ensure robust gameplay. Play now!",
+		liveDemo: "https://shahir-47.github.io/Battleship/",
+		sourceCode: "https://github.com/Shahir-47/Battleship",
+	},
+	{
+		image: Weather,
+		title: "Weather App",
+		description:
+			"Weather Whiz is a web app providing current weather, hourly & 2-day forecasts for different locations. Switch between imperial & metric units. Built with HTML, CSS, JS, Webpack, Babel, ESLint & Prettier. APIs: OpenWeather & Mapbox. Stay prepared with accurate weather data!",
+		liveDemo: "https://shahir-47.github.io/Weather-Whiz/",
+		sourceCode: "https://github.com/Shahir-47/Weather-Whiz",
+	},
+	{
+		image: Tictac,
+		title: "Tic Tac Toe",
+		description:
+			"Enjoy the classic Tic Tac Toe game with customizable markers, colors, and player names. Play against a friend or challenging AI. Responsive design, visually appealing interface. Source code for HTML, CSS, JavaScript implementation. Experience hours of interactive and addictive fun!",
+		liveDemo: "https://shahir-47.github.io/Tic-Tac-Toe/",
+		sourceCode: "https://github.com/Shahir-47/Tic-Tac-toe",
+	},
+	{
+		image: Calculator,
+		title: "Calculator",
+		description:
+			"This is a simple calculator application with keyboard support. It allows you to perform basic arithmetic operations such as addition, subtraction, multiplication, and division. It also includes additional features like calculating the factorial and exponentiation.",
+		liveDemo: "https://shahir-47.github.io/Calculator/",
+		sourceCode: "https://github.com/Shahir-47/Calculator",
+	},
+	{
+		image: Restaurant,
+		title: "Restaurant Page",
+		description:
+			"A web-based platform that provides information and features related to a fictional restaurant called the Galactic Cantina. The application allows users to explore various sections such as the home page, menu page, contact page, about us page, review page, and FAQ page.",
+		liveDemo: "https://shahir-47.github.io/Restaurant-Page/",
+		sourceCode: "https://github.com/Shahir-47/Restaurant-Page",
+	},
+	{
+		image: Etch,
+		title: "Etch a Sketch",
+		description:
+			"Experience the classic Etch-a-Sketch fun online! Draw and create masterpieces with this simple web application. Built using HTML, CSS, and JavaScript.",
+		liveDemo: "https://shahir-47.github.io/Etch-a-Sketch/",
+		sourceCode: "https://github.com/Shahir-47/Etch-a-Sketch/",
+	},
+];
+
+const ProjectItem = ({ image, title, description, liveDemo, sourceCode }) => (
+	<div className="project-item">
+		<img src={image} alt={title} />
+		<div className="project-item-div">
+			<h1>{title}</h1>
+			<p>{description}</p>
+			<div className="project-link-div">
+				<a href={liveDemo} target="_blank" rel="noopener noreferrer">
+					Live Demo
+				</a>
+				<a href={sourceCode} target="_blank" rel="noopener noreferrer">
+					Source Code
+				</a>
+			</div>
+		</div>
+	</div>
+);
+ProjectItem.propTypes = {
+	image: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	liveDemo: PropTypes.string.isRequired,
+	sourceCode: PropTypes.string.isRequired,
+};
+
 const Projects = () => {
-	return <div>Projects list will go here.</div>;
+	return (
+		<div className="project-div">
+			<h1>Projects</h1>
+			<div className="project-container">
+				{projectsData.map((project, index) => (
+					<ProjectItem key={index} {...project} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
